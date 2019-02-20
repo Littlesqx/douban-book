77347b418b3739a6dce09fcc059e0e3961d6d219
Littlesqx <Littlesqx@gmail.com>
2019-02-20 11:56:52 +0800

    Add laravel service

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the douban-book.
+ *
+ * (c) littlesqx <littlesqx@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Littlesqx\Book;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(Application::class, function () {
+           return new Application();
+        });
+        $this->app->alias(Application::class, 'douban-book');
+    }
+
+    public function provider()
+    {
+        return [Application::class, 'douban-book'];
+    }
+}
